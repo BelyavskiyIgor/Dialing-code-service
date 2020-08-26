a64595b4e7f7fef220f69121841e5812462adc0e
Igor Belyavskiy <belyavskiyi@gmail.com>
2020-08-26 20:55:22 +0300

    Добавлено кеширование. С задержкой для демонстрации  работы.

--- src/main/java/ru/Belyavskiy/PhoneCode/Repository/ExternalCountryRepositoryImpl.java
+++ src/main/java/ru/Belyavskiy/PhoneCode/Repository/ExternalCountryRepositoryImpl.java
@@ -1,5 +1,6 @@
 package ru.Belyavskiy.PhoneCode.Repository;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import ru.Belyavskiy.PhoneCode.Entity.Country;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -18,61 +20,45 @@ public class ExternalCountryRepositoryImpl implements CountryRepository{
 
     private Map<String, String> countryNames = new HashMap<>();
     private Map<String, String> countryCodes = new HashMap<>();
-
     private RestTemplate restTemplate;
-
-
     public ExternalCountryRepositoryImpl(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
-
-
-
-        //Подготавливаем возвращаемый тип  запроса
-        ParameterizedTypeReference<Map<String, String>> responseType = new ParameterizedTypeReference<Map<String, String>>() {};
-
-        // делаем запрос
-        ResponseEntity<Map<String, String>> response = restTemplate.exchange("names.json", HttpMethod.GET, null, responseType);
-
-        //результат  запроса  кладем в  мапу
-        countryNames = response.getBody();
-
-        // делаем запрос
-        response = restTemplate.exchange("phone.json", HttpMethod.GET, null, responseType);
-
-        //результат  запроса  кладем в  мапу
-        countryCodes = response.getBody();
-
-        System.out.println(countryNames.size() + " " + countryCodes.size());
+        updateCountries();
 
     }
+    private void updateCountries(){
 
+        ParameterizedTypeReference<Map<String, String>> responseType = new ParameterizedTypeReference<Map<String, String>>() {};
+        ResponseEntity<Map<String, String>> response = restTemplate.exchange("names.json", HttpMethod.GET, null, responseType);
+        countryNames = response.getBody();
+        response = restTemplate.exchange("phone.json", HttpMethod.GET, null, responseType);
+        countryCodes = response.getBody();
+    }
 
     @Override
+    @Cacheable(cacheNames = "recordsCache", key = "#countryName")
     public List<Country> findByCountryStartsWith(String countryName) {
 
-        // подготовить предикат
+        try {
+            TimeUnit.SECONDS.sleep(10);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
         Predicate<Map.Entry<String, String>> filter = entry -> entry.getValue().toLowerCase().startsWith(countryName.toLowerCase());
-
-        // профильтровать мапу по предикату
         List<String> filteredKeys = countryNames.entrySet().stream().filter(filter).map(Map.Entry::getKey).collect(Collectors.toList());
-        // и собрать список ключей
 
-        // превратить список ключей в список сущностей
         return filteredKeys.stream().map(key -> formCountry(key)).collect(Collectors.toList());
     }
 
     private Country formCountry(String country){
-        //Если указали неправильный  индефикатор
+
         if(!countryNames.containsKey(country)) throw new IllegalArgumentException();
-
-
         Country countryEntity = new Country();
-
         countryEntity.setName(country);
-        //взяли из мапы значения(название страны)
         countryEntity.setCountry(countryNames.get(country));
         countryEntity.setCode(countryCodes.get(country));
-
         return countryEntity;
     }
 
